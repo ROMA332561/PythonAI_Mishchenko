0ee33adcc44a5319edd754e021195e173c29b467
c2221 <romanmischenko3@gmail.com>
2025-03-15 17:23:01 +0200

    Lesson 3. Working with multiple classes. Creating a class person on a class car with methods to add a passenger and display a list of passengers

---
+++ lesson 2.py
@@ -0,0 +1,16 @@
+class Student:
+    amount_of_student = 0
+    print("Hi")
+    def __init__(self, scholarship = 50 ):
+        self.height = 170
+        print("I am alive!")
+        Student.amount_of_student +=1
+        self . scholarship = scholarship
+        self . scholarship += 100
+
+
+        def __srt__(selft):
+            return f"I am a Student. My nane is {self .name}"
+
+        def add_scholarship(slef):
+            slef
