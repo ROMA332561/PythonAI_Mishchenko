c9f0b55e2fc57c4e04dbda0569c25248a442bae1
c2221 <romanmischenko3@gmail.com>
2025-03-22 16:32:15 +0200

    Lesson 4. Class inheritance

---
+++ lesson.4.py
@@ -0,0 +1,55 @@
+# class Human:
+#     yheight = 170
+#
+# class Student(Human):
+#     satiety 0
+#
+# class Worker (Human):
+#     satiety 100
+#
+# h = Human()
+# s Student()
+# W = Worker()
+#
+# print(h.height)
+# print("-"*28)
+# print(s.satiety)
+# print(s.height)
+# print("*20)
+# point(w.satiety)
+# print(w.height)
+
+class Grandparent:
+    height 178
+    satiety 100
+    age = 60
+
+class Parent(Grandparent):
+    age 48
+    def __int__(self):
+class Child(Parent):
+height = 50
+    print(f"height (self.height)")
+    print("satiety (self.satiety)")
+    print(flage = [self.age)")
+
+nich Child()
+
+
+class HelloWorld (Hello):
+    de__init__ (self):
+super(). Inst()
+print("World")
+hw HelloWorld()
+class Computer:
+def calculate(self):
+print("Calculating...")
+class Dispaly:
+def diaplay(self):
+print("I diaplay the image on the screen...")
+class SmartPhone (Computer Dispaly):
+def print_info(self):
+print(self.diaplay())
+print(self.calculate())
+sp SmartPhone
+sp.print info()
