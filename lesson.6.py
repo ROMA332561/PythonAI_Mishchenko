0d0567edbc9b32a498b33c8af91c4a9a2fb0a89f
c2221 <romanmischenko3@gmail.com>
2025-03-22 18:01:31 +0200

    Lesson 6.

---
+++ lesson.6.py
@@ -0,0 +1,19 @@
+# try:
+#     numerator = int (input("ведіть чесельник:"))
+#     denominator = int(input("введіть знаменик"))
+#     print(numerator / denominator)
+# except ZeroDivisionError:
+#     print("ілення на 0 неможливе")
+# except ValueError:
+#     print("ведіть дані не є число")
+#
+
+import warnings
+warnings.simplefilter("ignore", SyntaxWarning)
+warnings.simplefilter("always" Importlarning)
+
+warnings.warn("Warning, no code here", SyntaxWarning)
+try:
+    warnings.warn("Warning, module not import", Importlarning)
+except:Exception
+print("Warning")
