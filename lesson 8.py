f2ace286d6c97317aa43832b1c7e590d606247d5
c2221 <romanmischenko3@gmail.com>
2025-03-29 17:52:27 +0200

    Lesson 8.

---
+++ lesson 8.py
@@ -0,0 +1,9 @@
+import logging
+
+Logging.hasicConfig(vel-logging.DEBUG,ilenames Logs.log, filemode
+formate "We have next Logging message: (asctime)s:%(levelhame)s
+(nessage)s"
+try:
+    print(18/8)
+except Exception:
+    logging.exception("Exception)
