04a06231ce34867c70f5d7498ab776515a7576b5
c2221 <romanmischenko3@gmail.com>
2025-03-29 16:05:44 +0200

    Lesson 7.

---
+++ lesson.7.py
@@ -0,0 +1,29 @@
+# import random
+# my_list = []
+#
+# for i in range(10):
+#     my_list.append(random.randint(-10,10))
+#
+# print(my_list)
+# print("-"*20)
+#
+# itrator = iter(my_list)
+# print(itrator)
+# print(next(itrator))
+# print(next(itrator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print("-"*20)
+#
+# iterator = iter(my_list)
+#
+# for elem in iterator:
+#     print(elem)
+
